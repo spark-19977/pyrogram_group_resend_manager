7d8e95ab269ad56383c3a8b7f967111ef3b1e5d6
vlad <akudovich_vlad@mail.ru>
2023-10-13 19:55:44 +0300

    incoming filter and + in regex

--- pyrogram_client/src/client.py
+++ pyrogram_client/src/client.py
@@ -41,38 +41,41 @@ class Application:
             async with Base.session() as session:
                 keywords = await session.scalars(select(Keyword).filter_by(chat_id=message.chat.id))
             for keyword in keywords:
-                text = message.text
-                _keyword = keyword.keyword
                 try:
-                    text = message.text.lower()
+                    text = message.text
+                    _keyword = keyword.keyword
+                    try:
+                        text = message.text.lower()
+                    except Exception:
+                        pass
+
+                    if '+' in _keyword and _keyword in text:
+                        self.scheduler.add_job(answer_in, trigger='date',
+                                               run_date=datetime.now() + timedelta(seconds=keyword.answer_in_seconds),
+                                               kwargs=dict(client=self.client, answer=keyword.answer,
+                                                           chat_id=message.chat.id, mess_id=message.id))
+                        if keyword.chat.one_time_answer:
+                            async with Base.session() as session:
+                                await session.execute(update(Chat).filter_by(id=message.chat.id).values(is_active=False))
+                                await session.commit()
+                        break
+
+                    try:
+                        _keyword = _keyword.replace('+', '\\+')
+                    except Exception:
+                        pass
+                    if re.search(fr'\b{_keyword}\b', text):
+                        self.scheduler.add_job(answer_in, trigger='date',
+                                               run_date=datetime.now() + timedelta(seconds=keyword.answer_in_seconds),
+                                               kwargs=dict(client=self.client, answer=keyword.answer,
+                                                           chat_id=message.chat.id, mess_id=message.id))
+                        if keyword.chat.one_time_answer:
+                            async with Base.session() as session:
+                                await session.execute(update(Chat).filter_by(id=message.chat.id).values(is_active=False))
+                                await session.commit()
+                        break
                 except Exception:
                     pass
-
-                if '+' in _keyword and _keyword in text:
-                    self.scheduler.add_job(answer_in, trigger='date',
-                                           run_date=datetime.now() + timedelta(seconds=keyword.answer_in_seconds),
-                                           kwargs=dict(client=self.client, answer=keyword.answer,
-                                                       chat_id=message.chat.id, mess_id=message.id))
-                    if keyword.chat.one_time_answer:
-                        async with Base.session() as session:
-                            await session.execute(update(Chat).filter_by(id=message.chat.id).values(is_active=False))
-                            await session.commit()
-                    break
-
-                try:
-                    _keyword = _keyword.replace('+', '\\+')
-                except Exception:
-                    pass
-                if re.search(fr'\b{_keyword}\b', text):
-                    self.scheduler.add_job(answer_in, trigger='date',
-                                           run_date=datetime.now() + timedelta(seconds=keyword.answer_in_seconds),
-                                           kwargs=dict(client=self.client, answer=keyword.answer,
-                                                       chat_id=message.chat.id, mess_id=message.id))
-                    if keyword.chat.one_time_answer:
-                        async with Base.session() as session:
-                            await session.execute(update(Chat).filter_by(id=message.chat.id).values(is_active=False))
-                            await session.commit()
-                    break
             logging.info('callback processed')
 
     async def start_loop(self):
